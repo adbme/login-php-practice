ca3e61d286be0348ed75f365ca421115e8eeafb9
Adil <adil.bouamrioune@gmail.com>
2023-03-07 20:38:01 +0100

    read me, index php, main css. images, init project

---
+++ README.md
@@ -0,0 +1,4 @@
+# php practice 1 / login
+
+
+- Mini login to the portal of my projects
---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+?>
+<header><h1>php practice 1 / login</h1> <img src="../images/pig.gif" alt=""></header>
+
+
+<link rel="stylesheet" href="/style/main.css
+">
+
+<div class="container">
+
+  <div class="form">
+    <form>
+      <div class="form-group">
+        
+      <div class="input-div"></div>
+        <img src="/images/user.png" alt="Icone utilisateur" />
+        <input type="text" name="username" placeholder="Nom d'utilisateur" />
+      </div>
+      <div class="form-group">
+
+    <div class="input-div"></div>
+
+        <img src="/images/password.png" />
+        <input type="password" name="password" placeholder="Mot de passe" />
+      </div>
+      <button type="submit">Se connecter</button>
+    </form>
+    <div class="signup-link">
+      <a href="#">Créer un compte</a>
+    </div>
+  </div>
+</div>
+
+
